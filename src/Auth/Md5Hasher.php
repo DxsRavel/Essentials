6c164aed570b8134a69a6ae9cf582c49b95b6b32
Angello Cruz <angellocruz0911@gmail.com>
2025-11-06 15:11:30 -0500

    FIX INFO

--- src/Auth/Md5Hasher.php
+++ src/Auth/Md5Hasher.php
@@ -6,15 +6,18 @@ use Illuminate\Contracts\Hashing\Hasher as HasherContract;
 class Md5Hasher implements HasherContract {
 
 	protected $rounds = 10;
-
+    public function info($hashedValue)
+    {
+        return password_get_info($hashedValue);
+    }
 	public function make($value, array $options = array()){
 		return md5($value);
 	}
 	public function check($value, $hashedValue, array $options = array()){
 		return md5($value) == $hashedValue;
 	}
-	public function needsRehash($hashedValue, array $options = array()){		
+	public function needsRehash($hashedValue, array $options = array()){
 		return md5($hashedValue);
 	}
-	
+
 }
